402a8ee638d7254786d239b8453c2745bd475a6a
Christoffer Arvidsson <christoffer@arvidson.nu>
2021-09-25 17:32:36 +0200

    Finish boltzmann machine implementation

--- boltzmann/src/model.py
+++ boltzmann/src/model.py
@@ -4,36 +4,69 @@ import numpy as np
 class BoltzmannMachine:
     def __init__(self, n_visible, n_hidden, seed=None):
         self.rng = np.random.default_rng(seed)
-        self.weights = self.rng.normal(0, (1/np.sqrt(n_hidden)), size=(n_visible, n_hidden))
-        self.v_thresholds = self.rng.uniform(size=(n_visible,))
-        self.h_thresholds = self.rng.uniform(size=(n_hidden,))
+        self.weights = self.rng.normal(0, 1, size=(n_visible, n_hidden))
+        self.v_thresholds = np.zeros((n_visible,))
+        self.h_thresholds = np.zeros((n_hidden,))
 
         self.v_state = np.zeros((n_visible,), dtype=float)
         self.h_state = np.zeros((n_hidden,), dtype=float)
 
+    @staticmethod
+    def local_field_hidden(state, weights, thresholds):
+        # print('Hidden', state.shape, weights.shape, thresholds.shape)
+        return (state.T @ weights).T - thresholds
+
+    @staticmethod
+    def local_field_visible(state, weights, thresholds):
+        # print('Visible', state.shape, weights.shape, thresholds.shape)
+        return (state @ weights.T).T - thresholds
+
+    def hebbs_rule(self, local_field):
+        p_b = (1+np.exp(-2*local_field))**-1
+        r = self.rng.random(local_field.shape)
+        new_state = np.zeros_like(local_field)
+        new_state[r < p_b] = 1
+        new_state[r >= p_b] = -1
+        return new_state
+
     def iteration_step(self):
-        b_visible =  self.h_state @ self.weights.T - self.v_thresholds
-        r_visible = self.rng.random(b_visible.shape)
-        self.v_state[r_visible < b_visible] = 1
-        self.v_state[r_visible >= b_visible] = -1
+        b_v = self.local_field_visible(self.h_state, self.weights, self.v_thresholds)
+        self.v_state = self.hebbs_rule(b_v)
+        b_h = self.local_field_hidden(self.v_state, self.weights, self.h_thresholds)
+        self.h_state = self.hebbs_rule(b_h)
 
-        b_hidden = self.weights.T @ self.v_state - self.h_thresholds
-        r_hidden = self.rng.random(b_hidden.shape)
-        self.h_state[r_hidden < b_hidden] = 1
-        self.h_state[r_hidden >= b_hidden] = -1
-
-    def run_cd_k(self, x, k=100, learning_rate=0.1):
+    def generate(self, x, iterations=100):
         self.v_state = x
 
-        b_hidden = self.weights.T @ x - self.h_thresholds
-        avg_data = np.tanh(b_hidden) * x[:,None]
-        for t in range(k):
+        b_h0 = self.local_field_hidden(self.v_state, self.weights, self.h_thresholds)
+        self.h_state = self.hebbs_rule(b_h0)
+        patterns = np.zeros((iterations, x.shape[0]), dtype=int)
+        for i in range(iterations):
             self.iteration_step()
+            patterns[i] = self.v_state
 
-        avg_model = np.tanh(self.weights.T @ self.v_state - self.h_thresholds) * self.v_state[:, None]
-        dW = learning_rate * (avg_data - avg_model) * self.v_state[:, None]
-        dT_visible = -learning_rate * (x - self.v_state)
-        dT_hidden = -learning_rate * (np.tanh(b_hidden) - np.tanh(self.weights.T @ self.v_state - self.h_thresholds))
+        return patterns
+
+    def run_cd_k(self, xs, k=100, learning_rate=0.1):
+        dW = np.zeros_like(self.weights)
+        dT_visible = np.zeros_like(self.v_thresholds)
+        dT_hidden = np.zeros_like(self.h_thresholds)
+        for x in xs:
+            self.v_state = x
+
+            b_h0 = self.local_field_hidden(self.v_state, self.weights, self.h_thresholds)
+            self.h_state = self.hebbs_rule(b_h0)
+
+            for t in range(k):
+                self.iteration_step()
+
+            b_hk = self.local_field_hidden(self.v_state, self.weights, self.h_thresholds)
+
+            avg_data = x[:,None] @ np.tanh(b_h0)[None,:]
+            avg_model = self.v_state[:,None] @ np.tanh(b_hk)[None,:]
+            dW += learning_rate * (avg_data - avg_model)
+            dT_visible -= learning_rate * (x - self.v_state)
+            dT_hidden -= learning_rate * (np.tanh(b_h0) - np.tanh(b_hk))
 
         self.weights += dW
         self.v_thresholds += dT_visible
